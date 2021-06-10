66d82db7296ffed2c0e3c7dfc0c010077b74c748
anhtudo97 <anhtudo97@gmail.com>
2021-06-10 22:50:38 +0700

    chore: update code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,7 +9,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// client side rendered app: react (cra)
+// -> database which is firebase and
+// -> react-loading-skeleton
+// tailwind
+
+// architecture
+// -> components, constants, context, helpers, lib( firebase is going to live in here), services (firebase function is here)
