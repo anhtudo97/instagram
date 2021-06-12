0b9b2286e11970b91cb7441b08eb93f52015b560
anhtudo97 <anhtudo97@gmail.com>
2021-06-12 17:23:17 +0700

    feat: update config

--- src/index.js
+++ src/index.js
@@ -13,11 +13,3 @@ ReactDOM.render(
   </FirebaseContext.Provider>,
   document.getElementById('root')
 );
-
-// client side rendered app: react (cra)
-// -> database which is firebase and
-// -> react-loading-skeleton
-// tailwind
-
-// architecture
-// -> components, constants, context, helpers, lib( firebase is going to live in here), services (firebase function is here)
