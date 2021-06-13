2956e9da53cff8ba15c68c9da2482bb413857d19
anhtudo97 <anhtudo97@gmail.com>
2021-06-13 17:03:40 +0700

    chore: update services

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,3 +9,44 @@ export const doesUsernameExist = async (username) => {
 
   return result.docs.length > 0;
 };
+
+export const getUserByUserId = async (userId) => {
+  const result = await firebase.firestore().collection('users').where('userId', '==', userId).get();
+
+  const user = result.docs.map((doc) => ({
+    ...doc.data(),
+    docId: doc.id
+  }));
+
+  return user;
+};
+
+export const getPhotos = async (userId, following) => {
+  // [5,4,2] => following
+  const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', 'in', following)
+    .get();
+
+  const userFollowedPhotos = result.docs.map((photo) => ({
+    ...photo.data(),
+    docId: photo.id
+  }));
+
+  const photosWithUserDetails = await Promise.all(
+    userFollowedPhotos.map(async (photo) => {
+      let userLikedPhoto = false;
+      if (photo.likes.includes(userId)) {
+        userLikedPhoto = true;
+      }
+      // photo.userId = 2
+      const user = await getUserByUserId(photo.userId);
+      // raphael
+      const { username } = user[0];
+      return { username, ...photo, userLikedPhoto };
+    })
+  );
+
+  return photosWithUserDetails;
+};
