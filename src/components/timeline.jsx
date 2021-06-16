b15b46c60afc5e92674778e512bc60b06608c5a9
anhtudo97 <anhtudo97@gmail.com>
2021-06-16 23:18:55 +0700

    feat: timeline

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,10 +1,20 @@
-const Timeline = () => {
-  const v = 1;
-  return (
-    <>
-      <p>Timeline</p>
-    </>
-  );
-};
+import { useContext } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import LoggedInUserContext from '../context/logged-in-user';
+import usePhotos from '../hooks/use-photos';
+import Post from './Post';
 
-export default Timeline;
+export default function Timeline() {
+  const { user } = useContext(LoggedInUserContext);
+  const { photos } = usePhotos(user);
+
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      )}
+    </div>
+  );
+}
