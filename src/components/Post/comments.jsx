5d2ede9982e572ce7d59e6bf67de18c184952a89
anhtudo97 <anhtudo97@gmail.com>
2021-06-16 23:18:46 +0700

    feat: post

---
+++ src/components/Post/comments.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { formatDistance } from 'date-fns';
+import { Link } from 'react-router-dom';
+import AddComment from './add-comment';
+
+const Comments = ({ docId, comments: allComments, posted, commentInput }) => {
+  const [comments, setComments] = useState(allComments);
+  const [commentsSlice, setCommentsSlice] = useState(3);
+
+  const showNextComments = () => {
+    setCommentsSlice(commentsSlice + 3);
+  };
+
+  return (
+    <>
+      <div className="p-4 pt-1 pb-4">
+        {comments.slice(0, commentsSlice).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span>{item.comment}</span>
+          </p>
+        ))}
+        {comments.length >= 3 && commentsSlice < comments.length && (
+          <button
+            className="mb-1 text-sm cursor-pointer text-gray-base focus:outline-none"
+            type="button"
+            onClick={showNextComments}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                showNextComments();
+              }
+            }}
+          >
+            View more comments
+          </button>
+        )}
+        <p className="mt-2 text-xs uppercase text-gray-base">
+          {formatDistance(posted, new Date())} ago
+        </p>
+      </div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
+    </>
+  );
+};
+
+Comments.propTypes = {
+  docId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
+  posted: PropTypes.number.isRequired,
+  commentInput: PropTypes.object.isRequired
+};
+export default Comments;
